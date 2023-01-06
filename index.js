fbdc10aebe826f7401669647b22e89a03ca22717
Philip Demand <philipdemand@yahoo.com>
2023-01-05 20:59:50 -0500

    commit tree 5be1068f7c798a524576e1a8d2b5529c25f6f7ec

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  let fare
+  if (rideLength <= 400) {
+    fare = 'This one is on me!'
+  } else if (rideLength > 400 && rideLength <= 2000) {
+    fare = 'That will be twenty bucks.'
+  } else if (rideLength > 2000 && rideLength <= 2500) {
+    fare = 'I will gladly take your thirty bucks.'
+  } else {
+    fare = 'No can do.'
+  }
+  return fare
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityDest){
+  let message = cityDest === "NYC" ? "Ok, sounds good." : "No go.";
+  return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generosity){
+  switch(generosity) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
